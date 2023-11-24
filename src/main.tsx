8daed78d374175af3144e3d512c3a898afd62e21
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-11-24 15:25:20 +0200

    add modern-normalize

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "modern-normalize/modern-normalize.css";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store.ts";
 import { PersistGate } from "redux-persist/integration/react";
